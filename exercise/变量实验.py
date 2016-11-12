b0ec1628b695df43fbb8a455aed1694f9166a60d
AncientDragon <mr_ace@126.com>
2016-11-13 00:46:28 +0800

    mkdir for exercise

---
+++ exercise/变量实验.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Author: Python
+# @Date:   2016-11-13 00:15:32
+# @Last Modified by:   AncientDragon
+# @Last Modified time: 2016-11-13 00:36:13
+a=[1,2,3,'asdb',5,6,7,8,9]
+print a
+print a[0]
+print a[3][3]
+print a[3:8]
+print a[3:]
+print a[1:8:3]
+print a[0:3]
+print a[:3]
