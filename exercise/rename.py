1402988d008ffdf9bd5f49f75ae8e5b68c82427d
AncientDragon <mr_ace@126.com>
2018-02-23 13:07:00 +0800

    os.rename_0 re.findall

--- exercise/rename.py
+++ exercise/rename.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
-# _*_ coding: utf-8 _*_
+# -*- coding: utf-8 -*-
 # @Time    : 2/23/2018 11:04 AM
 # @Author  : Apy
 # @File    : rename.py
 
+
+###rename
 import os
-os.rename('E:/12345','E:/123456')
+
+#print "E partion include: %s "%os.listdir('E:/')
+#os.rename('E:/12345', 'E:/123456')
+#print "哈哈哈"
+
+
+###re.findall
+import re
+
+dir_path='E:/a/'
+for file in os.listdir(dir_path):
+    print file
+    newfile=re.findall(r"\d+",file)
+    a=newfile[0]
+    os.rename (dir_path+file,dir_path+a+".txt")
+    #print type(a)
+os.listdir('E:/')
