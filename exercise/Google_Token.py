d9ed87f08a7c0eb417c60a5430dc377d2f8028bb
AncientDragon <mr_ace@126.com>
2018-03-02 18:16:16 +0800

    modify Google_Token.py

--- exercise/Google_Token.py
+++ exercise/Google_Token.py
@@ -21,16 +21,20 @@ def Main():
     xsh.Screen.Send(fin_token)
     ###enter token
     xsh.Screen.Send('\r')
-    IP = re.findall(r"\d+", str(xsh.Session.Path))
 
-    ###enter ip addr
-    for i in range(0, 3):
-        xsh.Screen.Send(IP[i])
-        xsh.Screen.Send('.')
-    xsh.Screen.Send(abs(IP[3]))
-    xsh.Screen.Send('\r')
+    ###get IP addr
+    IP = re.findall(r"\d+", str(xsh.Session.Path))
+    if len(IP) == 5:
+        ###enter IP addr
+        for i in range(1, 4):
+            xsh.Screen.Send(IP[i] + '.')
+        xsh.Screen.Send(IP[4] + '\r')
+    else:
+        ###enter ip addr
+        for i in range(0, 3):
+            xsh.Screen.Send(IP[i] + '.')
+        xsh.Screen.Send(IP[3] + '\r')
     ###sudo su
     xsh.Screen.Synchronous = True
-    xsh.Session.Sleep(2500)
-    xsh.Screen.Send("sudo su")
-    xsh.Screen.Send('\r')
+    xsh.Session.Sleep(3500)
+    xsh.Screen.Send("sudo su" + '\r')
--- exercise/len.py
+++ exercise/len.py
@@ -11,11 +11,27 @@
 # print len(str(a))
 # print len(b)
 
-c = "C:\Users\Apy\Documents\NetSarang\Xshell\Sessions\Mylc\10.150.20.1-UE-nginx-ftp.xsh"
+c = "C:\Users\Apy\Documents\NetSarang\Xshell\Sessions\Mylc\10.150.20.1.1-UE-nginx-ftp.xsh"
 
 # IP = re.findall(r"\d+", c)
-for i in range(0, 7):
-    print(i)
-    print('.')
-print(i)
-print('\r')
+#for i in range(0, 7):
+#    print(i)
+#    print('.')
+#print(i)
+#print('\r')
+
+import re
+import onetimepass as otp
+#my_secret = 'ZYPRDAEJVPBMFGUA'
+#my_token = otp.get_totp(my_secret)
+## print "my_token is %s"%my_token
+#
+#if len(str(my_token)) == 6:
+#    fin_token = my_token
+#else:
+#    fin_token = "0" + str(my_token)
+#xsh.Screen.Send(fin_token)
+####enter token
+#xsh.Screen.Send('\r')
+IP = re.findall(r"\d+.", c)
+print len(IP)
