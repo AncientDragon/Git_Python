661f2872602c86294c4b0206f14c06796d675e2e
Ancientdragon <mr_ace@126.com>
2019-02-12 14:57:46 +0800

    20190212

--- exercise/header.py
+++ exercise/header.py
@@ -3,3 +3,9 @@
 # @Time    : 2/24/2018 2:51 PM
 # @Author  : Apy
 # @File    : header.py
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : ${DATE} ${TIME}
+# @Author  : Apy
+# @File    : ${NAME}.py
